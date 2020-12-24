39d73465c11b789fb27b5293e33fb32024c4c274
yuliao <spyfour30810@gmail.com>
2020-12-24 20:35:04 +0800

    [modified] upperlimb_exercise

--- app/src/main/java/com/example/fitit/Mission.java
+++ app/src/main/java/com/example/fitit/Mission.java
@@ -2,13 +2,41 @@ package com.example.fitit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Mission extends AppCompatActivity {
-
+    private Button upperlimb_btn1, lowerlimb_btn1, softness_btn1, endurance_btn1, back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission);
+        findObject();
+        clickBtnEvent();
+
+    }
+    public void clickBtnEvent(){
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { finish(); }
+        });
+
+        upperlimb_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Mission.this, Upper1_exercise.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void findObject(){
+        back_btn = findViewById(R.id.back_btn);
+        upperlimb_btn1 = findViewById(R.id.upperlimb_btn1);
+        lowerlimb_btn1 = findViewById(R.id.lowerlimb_btn1);
+        softness_btn1 = findViewById(R.id.softness_btn1);
+        endurance_btn1 = findViewById(R.id.endurance_btn1);
     }
 }
--- app/src/main/java/com/example/fitit/Upper1_exercise.java
+++ app/src/main/java/com/example/fitit/Upper1_exercise.java
@@ -3,12 +3,90 @@ package com.example.fitit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Upper1_exercise extends AppCompatActivity {
-
+    private Button back_btn, start_btn;
+    private ImageView exercise_pic;
+    private TextView clock_txt, exercise_txt;
+    private Timer timer;
+    private int sec = 60, min = 4, num=0;
+    private int[] Img = { R.drawable.exercise_upper1, R.drawable.exercise_upper2,
+                                R.drawable.exercise_upper3,R.drawable.exercise_upper4,
+                                R.drawable.exercise_upper3, R.drawable.exercise_upper5 };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upper1_exercise);
+        findObject();
+        clickBtnEvent();
+    }
+    public void findObject(){
+        exercise_pic = findViewById(R.id.exercise_pic);
+        back_btn = findViewById(R.id.back_btn);
+        start_btn = findViewById(R.id.start_btn);
+        clock_txt = findViewById(R.id.clock_txt);
+        exercise_txt = findViewById(R.id.exercise_txt);
+    }
+    public void clickBtnEvent(){
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { finish(); }
+        });
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_btn.setVisibility(View.INVISIBLE);
+                countDown();
+            }
+        });
+    }
+    public void countDown(){
+        new CountDownTimer(180000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                min = (int) (millisUntilFinished/60000);
+                sec = (int)(millisUntilFinished%60000)/1000;
+                changePicture();
+                if(sec>=0 && sec<10) {
+                    clock_txt.setText( String.valueOf(min)+":0"+String.valueOf(sec)); }
+                else {
+                    clock_txt.setText( String.valueOf(min)+":"+String.valueOf(sec));
+                }
+
+            }
+
+            public void onFinish() {
+                exercise_txt.setText("完成！");
+                clock_txt.setText("00:00");
+
+            }
+        }.start();
+    }
+    public void changePicture(){
+        if(min == 2 && sec >= 0 && sec <= 60){
+            exercise_txt.setText("擺動雙臂");
+            exercise_pic.setImageResource(Img[num]);
+            num++;
+            if(num == 2) {  num = 0; }
+        }
+        else if(min == 1 && sec >= 0 && sec <= 60){
+            exercise_txt.setText("手臂外舉");
+            exercise_pic.setImageResource(Img[num]);
+            num++;
+            if(num == 4) {  num = 2; }
+        }
+        else{
+            exercise_txt.setText("手臂上舉");
+            exercise_pic.setImageResource(Img[num]);
+            num++;
+            if(num == 6) {  num = 4; }
+        }
     }
 }
