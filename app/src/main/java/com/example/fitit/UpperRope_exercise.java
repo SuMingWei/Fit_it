e6b0b2a1cccf23b5ac9e4d74680d27417ecf9ff2
yuliao <spyfour30810@gmail.com>
2021-01-11 09:37:51 +0800

    [add] new upper rope exercise page

---
+++ app/src/main/java/com/example/fitit/UpperRope_exercise.java
@@ -0,0 +1,14 @@
+package com.example.fitit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpperRope_exercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upper_rope_exercise);
+    }
+}
