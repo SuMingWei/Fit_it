497390fa78a6db969cddd72c64aeeaa3b43de27c
yuliao <spyfour30810@gmail.com>
2021-09-13 01:06:39 +0800

    update teaching video popupwindow

--- app/src/main/java/com/example/fitit/MainActivity.java
+++ app/src/main/java/com/example/fitit/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.fitit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
